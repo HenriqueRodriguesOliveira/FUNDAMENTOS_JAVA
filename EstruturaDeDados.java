24a4cbdff2d3aa706ea293dea1fdf36d2b1b6069
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2024-02-27 10:28:42 -0300

    Estrutura de dados

---
+++ EstruturaDeDados.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstruturaDeDados {
+    public static void main(String[] args) {
+        // Lista
+        List<String> nomes = new ArrayList<>();
+        nomes.add("Henrique");
+        nomes.add("Rodrigues");
+        nomes.add("Elias");
+        // 0 -
+        // 1 -
+        // 2 -
+
+        System.out.println(nomes.get(0));
+        for( String nome : nomes) {
+            System.out.println("Nome e:" + nome);
+        }
+
+        nomes.forEach(nome -> System.out.println("O nome que apareceu foi "+ nome));
+    }
+}
