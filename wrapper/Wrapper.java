e4c0ecc515cf63624d8c59bd766ef1b2f18379a8
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2024-02-28 15:00:56 -0300

    Wrapper

---
+++ wrapper/Wrapper.java
@@ -0,0 +1,15 @@
+package wrapper;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        int tipoInt = 1;
+        float tipoFloat = 1;
+        boolean tipoBoolean = false;
+
+        Float tipFloat2 = 1F;
+        Boolean tipBoolean2 = false;
+        Integer tipoInteger = 1;
+
+        // tipoInteger.parseInt("1");
+    }
+}
